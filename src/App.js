baa82115c025973648439b934b0312e6c8f6f3e1
Nguyễn Văn Huy <huynvph04886@fpt.edu.vn>
2018-06-07 16:06:33 +0700

    te

--- src/App.js
+++ src/App.js
@@ -3,49 +3,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      data : [
-        {
-          id : 1,
-          name : 'huy',
-          age : 20
-        },
-        {
-          id : 2,
-          name : 'tung',
-          age : 20
-        },
-        {
-          id : 3,
-          name : 'tuan',
-          age : 20
-        },
-        {
-          id : 4,
-          name : 'trung',
-          age : 20
-        },
-        {
-          id : 5,
-          name : 'hung',
-          age : 20
-        }
-      ]
-    };
+    this.state = { repoNames: [] };
+  }
+
+  componentDidMount() {
+    let self = this;
+    fetch("https://api.github.com/repositories", {method: 'get'})
+      .then((response) => { return response.json(); })
+      .then((repos) => {
+        self.setState({ repoNames: repos.map((r) => { return r.name; })});
+      });
   }
 
   render() {
     return (
-         <div>
-            <table border="1">
-                {this.state.data.map((r, i) => {return <tr key={i}><td>{r['id']}</td><td>{r['name']}</td><td>{r['age']}</td></tr>})}
-            </table>
-        </div>
-        )
-    }
+      <ol>
+        {this.state.repoNames.map((r, i) => { return <li key={i}>{r}</li> })}
+      </ol>
+    )
   }
+}
 
-  export default App;
+export default App;
